3636b6f3471d41e16e1fcc2aeccaf46ce23c440d
0x240x23elu <52744492+0x240x23elu@users.noreply.github.com>
2021-01-17 12:19:32 +0530

    Huge List error fix

--- s3.py
+++ s3.py
@@ -19,56 +19,57 @@ count = 0
 
 for line in Lines: 
     ip = line.strip() 
-    r = requests.get(ip+".s3.amazonaws.com", allow_redirects=True, verify=False)
-   
+    try:
+        r = requests.get(ip+".s3.amazonaws.com", allow_redirects=True, verify=False)
+     
 
-    A = "AccessDenied" 
-    B = "NoSuchKey"
-    C = "NoSuchBucket"
-    D = "PermanentRedirect"
-    E = "200"
-    F = "ListBucketResult"
-    f = open('out.txt', 'a')
-    if A in r.text:
-             print("Bucket Found: ", ip, ":", A)
-             L = ["Bucket Found: ", ip, ":", A, '\n']
-             f.writelines(L)
-             f.close()
-            
-    elif B in r.text:
-             print("Bucket does not exist: ", B, ":", ip)
-             L = ["Bucket does not exist: ", B, ":", ip, '\n']
-             f.writelines(L)
-             f.close()
-             
-    elif C in r.text:
-             print("Bucket does not exist: ", C, ":", ip)
-             L = ["Bucket does not exist: ", C, ":", ip, '\n']
-             f.writelines(L)
-             f.close()
-    elif D in r.text:
-             print(D, ":", ip)
-             L = [D, ":", ip, '\n']
-             f.writelines(L)
-             f.close()
-    elif E in r.status_code:
-             print("Bucket Found: ", ip, ":", D)
-             L = ["Bucket Found: ", ip, ":", D, '\n']
-             f.writelines(L)
-             f.close()
-    elif F in r.text:
-             print("Bucket Found: ", ip, ":", F)
-             L = ["Bucket Found: ", ip, ":", F, '\n']
-             f.writelines(L)
-             f.close()
-    else:
-             print("Bucket Not found :", ip)
-             L = ["Bucket Not found :", ip, '\n']
-             f.writelines(L)
-             f.close()
+        A = "AccessDenied" 
+        B = "NoSuchKey"
+        C = "NoSuchBucket"
+        D = "PermanentRedirect"
+        E = "200"
+        F = "ListBucketResult"
+        f = open('out.txt', 'a')
+        if A in r.text:
+                 print("Bucket Found: ", ip, ":", A)
+                 L = ["Bucket Found: ", ip, ":", A, '\n']
+                 f.writelines(L)
+                 f.close()
+                
+        elif B in r.text:
+                 print("Bucket does not exist: ", B, ":", ip)
+                 L = ["Bucket does not exist: ", B, ":", ip, '\n']
+                 f.writelines(L)
+                 f.close()
                  
+        elif C in r.text:
+                 print("Bucket does not exist: ", C, ":", ip)
+                 L = ["Bucket does not exist: ", C, ":", ip, '\n']
+                 f.writelines(L)
+                 f.close()
+        elif D in r.text:
+                 print(D, ":", ip)
+                 L = [D, ":", ip, '\n']
+                 f.writelines(L)
+                 f.close()
+        elif E in r.status_code:
+                 print("Bucket Found: ", ip, ":", D)
+                 L = ["Bucket Found: ", ip, ":", D, '\n']
+                 f.writelines(L)
+                 f.close()
+        elif F in r.text:
+                 print("Bucket Found: ", ip, ":", F)
+                 L = ["Bucket Found: ", ip, ":", F, '\n']
+                 f.writelines(L)
+                 f.close()
+        else:
+                 print("Bucket Not found :", ip)
+                 L = ["Bucket Not found :", ip, '\n']
+                 f.writelines(L)
+                 f.close()
+                 
+    except requests.exceptions.RequestException as e:
+        # A serious problem happened, like an SSLError or InvalidURL
+        print("Error: {}".format(e))
                 
                  
-
- 
-
